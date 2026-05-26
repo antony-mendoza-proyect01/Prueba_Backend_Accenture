e43c1b1a5ac4237d8fa38d716a81c1fa988b0f5e
Ing. Antony Mendoza <107371155+antony-mendoza-proyect01@users.noreply.github.com>
2026-05-25 19:18:33 -0500

    creation of repository and model

---
+++ src/main/java/Prueba_Backend_Accenture/domain/model/Franchise.java
@@ -0,0 +1,18 @@
+package Prueba_Backend_Accenture.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Franchise {
+    private Long id;
+    private String name;
+    private List<Branch> branches;
+}
---
+++ src/main/java/Prueba_Backend_Accenture/infratructure/persistence/repository/BranchRepository.java
@@ -0,0 +1,13 @@
+package Prueba_Backend_Accenture.infratructure.persistence.repository;
+
+import Prueba_Backend_Accenture.domain.model.Branch;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface BranchRepository {
+    Mono<Branch> save(Branch branch);
+    Mono<Branch> findById(Long id);
+    Flux<Branch> findByFranchiseId(Long franchiseId);
+    Mono<Branch> update(Branch branch);
+    Mono<Boolean> existsById(Long id);
+}
