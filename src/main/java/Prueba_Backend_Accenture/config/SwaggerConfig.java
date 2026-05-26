02537aa04c72f3a38be23daa0c140622576cc1dd
Ing. Antony Mendoza <107371155+antony-mendoza-proyect01@users.noreply.github.com>
2026-05-25 19:47:35 -0500

    test completion

--- src/main/java/Prueba_Backend_Accenture/config/SwaggerConfig.java
+++ src/main/java/Prueba_Backend_Accenture/config/SwaggerConfig.java
@@ -1,4 +1,23 @@
 package Prueba_Backend_Accenture.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        return new OpenAPI().info(new Info()
+                .title("API Franquicias")
+                .version("1.0")
+                .description("Prueba técnica Accenture")
+                .contact(
+                        new Contact().name("Antony Mendoza").email("antoni-6191@hotmail.com"))
+        );
+    }
 }
